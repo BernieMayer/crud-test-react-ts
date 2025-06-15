a4ca024a57ca75f704aad54a669f41d4b9f3de50
Bernie Mayer <berniemayer94@gmail.com>
2025-06-15 13:00:42 -0600

    Add login UI

--- src/presentation/pages/Login/Login.tsx
+++ src/presentation/pages/Login/Login.tsx
@@ -1,9 +1,30 @@
+import { Button, Stack, TextField } from "@mui/material"
+
 function Login() {
 
 
   return (
-    <>
-      <h1>Login</h1>
+    <> 
+      <Stack direction="column" spacing={2} padding={2}   sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+          <TextField
+            id="outlined-password-input"
+            label="User Name"
+            type="text"
+        />
+        <TextField
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+        />
+      </Stack>
+
+      <Button variant="outlined">
+        Login
+      </Button>
     </>
   )
 }
