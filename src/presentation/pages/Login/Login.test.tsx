def1c96c2c0d437e11fe08903ea68cb1b1cdb33e
Bernie Mayer <berniemayer94@gmail.com>
2025-06-15 13:15:29 -0600

    Fix login test

--- src/presentation/pages/Login/Login.test.tsx
+++ src/presentation/pages/Login/Login.test.tsx
@@ -1,7 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import Login from './Login'
+import { MemoryRouter } from 'react-router-dom';
 
 test('displays Login', () => {
-  render(<Login />);
+  render( <MemoryRouter>
+    <Login />
+  </MemoryRouter>
+  );
   expect(screen.getByText(/Login/i)).toBeInTheDocument();
 });
