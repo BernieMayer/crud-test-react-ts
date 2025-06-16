0a5fe61c72fff82da9a0dca8591f59763ff196f6
Bernie Mayer <berniemayer94@gmail.com>
2025-06-15 21:23:19 -0600

    Integrate profle into the profile section

--- src/presentation/pages/Profile.tsx
+++ src/presentation/pages/Profile.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
+import { User } from "../../models/User";
+import UserForm from "../components/UserForm/UserForm"
+import UserStorage from "../../models/UserStorage";
+import { Button } from "@mui/material";
+
 function Profile() {
 
+  const [currentUser, setCurrentUser] = useState<User| null>(null);
+  const [currentMode, setCurrentMode] = useState<String>("profile")
+
+  useEffect(() => {
+    const currentUser = UserStorage.getCurrentUser();
+    setCurrentUser(currentUser);
+  }, []);
+
+  const editProfile = () => {
+    setCurrentMode("edit");
+  }
 
   return (
     <>
       <h1>Profile</h1>
+
+      { currentUser && <UserForm mode={currentMode === "profile" ? "profile" : "edit"} user={currentUser}  onValidSubmit={() => {setCurrentMode("profile")}}/> }
+
+
+        <Button 
+          type="button" 
+          onClick={() => editProfile()} 
+          variant="contained" 
+          color="primary"
+          disabled={currentMode!=="profile"}
+        >
+          Edit
+        </Button>
+        
     </>
   )
 }
